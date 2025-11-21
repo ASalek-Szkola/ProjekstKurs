a75d141fcf6e657af04d8b37a0d3e9fe6f77d36e
ASalek-Szkola <antoni.salek@tm1.edu.pl>
2025-11-21 12:19:17 +0100

    Mały test API

--- server.js
+++ server.js
@@ -5,18 +5,17 @@ const app = express();
 
 import { db_conn } from "./db.js";
 
-try {
-  const [results] = await db_conn.query(
-    'select * from users'
-  );
+app.get("/", async (req, res) => {
+    try {
+        const [results] = await db_conn.query(
+            'select * from users'
+        );
 
-  console.log(results);
-} catch (err) {
-  console.log(err);
-}
-
-app.get("/", (req, res) => {
-    
+        res.status(200).json(results);
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
 })
 
 app.listen(PORT, () => {
