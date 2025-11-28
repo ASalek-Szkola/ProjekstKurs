a9c20680bbf26e0fa38095e1304cfb8d875e42bc
= <=>
2025-11-28 13:21:30 +0100

    a

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    (async() => {
+      try{
+        const response = await fetch("http://localhost:3000/get-users");
+        if (!response.ok) throw new Error("Response not OK");
+        const json = await response.json();
+        console.log(json)
+      } catch(err) {
+        console.error(err)
+      };
+    })()
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button>
+          count is
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
@@ -28,6 +31,16 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>
+        <table>
+          <thead>
+            <th>id</th>
+            <th>name</th>
+            <th></th>
+            <th></th>
+          </thead>
+        </table>
+      </div>
     </>
   )
 }
